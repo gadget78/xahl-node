e1c4de7f79988ae27b72de9166830083b4de0134
Mick <35707644+gadget78@users.noreply.github.com>
2025-01-14 22:33:20 +0000

    bug

--- setup.sh
+++ setup.sh
@@ -887,7 +887,7 @@ FUNC_SETUP_UFW_PORTS(){
 
     # Get current SSH and xahau node port number, and unblock them
     SSH_PORT=$(sudo ss -tlpn | grep sshd | awk '{print$4}' | cut -d ':' -f 2 -s) || SSH_PORT=""
-    if [[ -z "$SSH_PORT"]]; then
+    if [[ -z "$SSH_PORT" ]]; then
         echo -e "current SSH port number detected as: ${BYELLOW}$SSH_PORT${NC}"
         sudo ufw allow $SSH_PORT/tcp
     else
